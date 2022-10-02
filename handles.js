26ce211a62872dc59cf1821a73f1fae5befa41bb
nicolassaint <nico@LAPTOP-9MT9BI43.localdomain>
2022-10-02 16:06:20 +0200

    fin tp

--- handles.js
+++ handles.js
@@ -1,6 +1,7 @@
 const url = require('url')
 const qs = require('querystring')
-var monJson = require('./about.json');
+const { readFileSync } = require('fs');
+
 
 // ./handles.js
 // Necessary imports
@@ -9,35 +10,38 @@ module.exports = {
     const route = url.parse(req.url)
     const path = route.pathname
     const params = qs.parse(route.query)
+    const user = JSON.parse(readFileSync("./about.json", 'utf-8'));
+    var i = 0;
 
     res.writeHead(200, { 'Content-Type': 'text/plain' });
 
     if (path === '/hello' && 'name' in params) {
 
       res.write('Hello ' + params['name'])
-      if (params['name'] === monJson.author) {
+
+      for (i = 0; i < user.length ; i++){
+
+      if (params['name'] === user[i].author) {
         res.write("\n\n\n")
-        res.write(monJson.title)
+        res.write(user[i].title)
         res.write("\n\n")
-        res.write(monJson.content)
+        res.write(user[i].content)
         res.write("\n")
-        res.write(monJson.date)
+        res.write(user[i].date)
         res.write("\n")
-        console.log(monJson)
-      }
-      else {
-        res.write('\n\n ERROR 404, page not found !')
-      }
+      }}
+       
 
     }
     else if (path === '/') {
-
-      res.write("En rentrant dans l'url hello?name=votre_nom, vous pourrez voir votre nom affiche,\n si votre nom est dans notre BDD, alors vos informations s'afficheront")
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write("En rentrant dans l'url <a href='/hello?name='>hello?name=votre_nom</a>, vous pourrez voir votre nom affiche,<br> si votre nom est dans notre BDD, alors vos informations s'afficheront")
 
     }
 
     else {
-      res.write('ERROR 404, page not found !')
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.write("ERROR 404 : Page not found !")
     }
 
     res.end();
