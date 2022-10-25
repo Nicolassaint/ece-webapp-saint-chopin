3f766054eedea1f96f96b2e7f8698d194dc1baa0
Thomas Chopin <thomas.chopin@edu.ece.fr>
2022-10-25 22:20:01 +0200

    feat: modification page contacts

--- lab4/components/layout/navbar.js
+++ lab4/components/layout/navbar.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div class="bg-gray-700">
+    <div class="bg-gray-400">
       <div class="mx-auto max-w-xl px-2 sm:px-6 lg:px-8">
         <div class="relative flex h-16 items-center justify-between">
           <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
@@ -11,28 +11,28 @@ export default function Navbar() {
               <div class="flex space-x-10">
                 <Link
                   href="/"
-                  classname="hover:bg-gray-700 text-gray-300 hover:text-white  px-3 py-2 rounded-md text-sm font-medium"
+                  classname="text-white hover:text-white  px-3 py-2 rounded-md text-sm font-medium"
                 >
                   Home
                 </Link>
 
                 <Link
                   href="/about"
-                  classname="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  classname="text-white hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   About
                 </Link>
 
                 <Link
                   href="/contacts"
-                  class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  class="text-white hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   Contacts
                 </Link>
 
                 <Link
                   href="/articles"
-                  class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  class="text-white hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   Articles
                 </Link>
