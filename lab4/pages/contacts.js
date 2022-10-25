9bc0cc94f99c430f8ece83fdde71770bc0486fbd
Thomas Chopin <thomas.chopin@edu.ece.fr>
2022-10-25 21:13:59 +0200

    feat: finition de conact

--- lab4/pages/contacts.js
+++ lab4/pages/contacts.js
@@ -7,32 +7,32 @@ import { Grid } from "@mui/material";
 
 const contacts = () => {
   return (
-    <Box
-    className="mt-6"
-    /*sx={{
-      marginTop: "6.5px",
-    }}*/>
+    <Box className="mt-6">
       <Grid item container justifyContent="center">
         <Grid xs={5}>
-          <h1>Contact 1</h1>
-          <h2>Nicolas Saint</h2>
-          <Image
-            src={mypic1}
-            alt="Nicolas Saint"
-            width="150px"
-            height="150px"
-          />
-
+          <h1 className="ml-2 text-blod text-2xl">Contact 1 : Nicolas Saint</h1>
+          <div classname="flex flex-wrap justify-center mt-24 w-2/12 overflow-hidden object-left">
+            <Image
+              src={mypic1}
+              classname=" h-5 shadow-lg "
+              alt="Nicolas Saint"
+            />
+          </div>
           <h3>Téléphone : 06 65 48 62 68</h3>
           <h3>Adresse : nicolas.saint78@gmail.com</h3>
         </Grid>
-        <Grid >
-        <h1>Contact 2</h1>
-        <h2>Thomas Chopin</h2>
-        <Image src={mypic2} alt="Thomas Chopin" width="150px" height="150px" />
-        <h3>Téléphone : 06 78 72 63 26</h3>
-        <h3>Adresse : tomchopin91@gmail.com</h3>
-      </Grid>
+        <Grid>
+          <h1 className="ml-2 text-blod text-2xl">Contact 2 : Thomas Chopin</h1>
+          <div classname="flex flex-wrap justify-center mt-24 w-2/12 right-12 overflow-hidden">
+            <Image
+              classname=" h-5 shadow-lg "
+              src={mypic2}
+              alt="Thomas Chopin"
+            />
+          </div>
+          <h3>Téléphone : 06 78 72 63 26</h3>
+          <h3>Adresse : tomchopin91@gmail.com</h3>
+        </Grid>
       </Grid>
     </Box>
   );
