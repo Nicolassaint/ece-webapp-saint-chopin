c57e089904880118d5646ba95b2b7edc1483dfad
thomas91600 <thomas.chopin@edu.ece.fr>
2022-12-14 23:59:19 +0100

    feat:new version of delete and design

--- Blog-ECE/pages/deletearticle.js
+++ Blog-ECE/pages/deletearticle.js
@@ -7,7 +7,7 @@ export default function Account({ session }) {
   const user = useUser()
   const [loading, setLoading] = useState(true)
   const [title, setTitle] = useState([])
-  const [tittle, setTittle] = useState(null)
+  const [titre, setTitre] = useState(null)
 
 useEffect(() => {
     getArticle()
@@ -36,11 +36,11 @@ useEffect(() => {
     }
   }
 
-  async function deleteArticle({ tittle }) {
+  async function deleteArticle({ titre }) {
     try {
       setLoading(true)
-      let { error } = await supabase.from('articles').delete().eq('title',tittle)
-      console.log(tittle)
+      let { error } = await supabase.from('articles').delete().eq('title',titre)
+      console.log(titre)
       if (error) throw error
       alert('Article deleted')
     } catch (error) {
@@ -52,28 +52,34 @@ useEffect(() => {
   }
 
   return (
-     <div classname="mr-5 ml-5">
-         {title.map((article) => (
-                   <div key={article.id_article}>
-                             <label className='text-center'>{article.title}</label>
-                   </div>
-                ))}
-
-       <div className='pt-4 text-center'>
-       <label >Tittle</label>
-        <input type="text" className="border-solid border-2 border-black w-60 rounded-md dark:bg-slate-800" onChange={(e) => setTittle(e.target.value)}/>
+    //  <div classname="mr-5 ml-5">
+    //      {title.map((article) => (
+    //                <div key={article.id_article}>
+    //                          <label className='text-center'>{article.title}</label>
+    //                </div>
+    //             ))}
+    //   </div>
+      <div>
+        <div className="mx-auto w-full max-w-2xl rounded-xl bg-white p-8 shadow dark:bg-neutral-800 border-solid border-2 border-black w-100 rounded-md">
+                <div className="text-center items-center justify-center text-10xl font-bold">
+                <label>All the articles you have written {user.id}</label>
+                {title.map((article) => (
+                            <div key={article.title}>
+                                      <button onClick={() => setTitre(article.title)}>{article.title}</button>
+                            </div> 
+                          ))}
+                  </div>
+        </div>
+        <div className='pt-2 text-center'>
+          <button
+            className="border-solid border-2 border-black w-60 rounded-md bg-blue-500 hover:bg-blue-700 text-white"
+            onClick={() => deleteArticle({titre})}
+            disabled={loading}
+          >
+            {loading ? 'Loading ...' : 'Delete'}
+          </button>
+        </div>
       </div>
-
-
-      <div className='pt-4'>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded block"
-          onClick={() => deleteArticle({tittle})}
-          disabled={loading}
-        >
-          {loading ? 'Loading ...' : 'Delete'}
-        </button>
-      </div>
-    </div>
   )
 }
+
