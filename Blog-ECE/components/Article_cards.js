f44d13af612117a25fd4d327089740861d146929
nicolassaint <nicolas.saint78@gmail.com>
2022-12-14 17:22:18 +0100

    feat: post comment in database if you're logged

--- Blog-ECE/components/Article_cards.js
+++ Blog-ECE/components/Article_cards.js
@@ -1,6 +1,65 @@
 import React from 'react'
+import { useSupabaseClient } from '@supabase/auth-helpers-react'
+import { useState, useEffect } from 'react'
 
-const Article_cards = (title, content) => {
+
+const Article_cards = (article) => {
+
+  console.log("article import : ",article)
+  const supabase = useSupabaseClient()
+  const [post, setPost] = useState([])
+  const [avatarUrl, setAvatarUrl] = useState(null)
+
+  useEffect(() => {
+    setPost(article)
+    console.log("post :", post)
+  }, [article])
+
+  function getYear(timestamp) {
+    var date = new Date(timestamp);
+    return date.getFullYear()
+  }
+
+  function getMonth(timestamp) {
+    var date = new Date(timestamp);
+    let month = date.getMonth() + 1;
+    if (month === 1) { month = "Jan" }
+    else if (month === 2) { month = "Feb" }
+    else if (month === 3) { month = "Mar" }
+    else if (month === 4) { month = "Apr" }
+    else if (month === 5) { month = "May" }
+    else if (month === 6) { month = "Jun" }
+    else if (month === 7) { month = "Jul" }
+    else if (month === 8) { month = "Aug" }
+    else if (month === 9) { month = "Sep" }
+    else if (month === 10) { month = "Oct" }
+    else if (month === 11) { month = "Nov" }
+    else if (month === 12) { month = "Dec" }
+
+    return month;
+  }
+
+  function getDay(timestamp) {
+    var date = new Date(timestamp);
+    return date.getDate()
+  }
+
+  async function downloadImage(path, i) {
+    try {
+      console.log(path)
+      const { data, error } = await supabase.storage.from('avatars').download(path)
+      console.log(data)
+      if (error) {
+        throw error
+      }
+      const url = URL.createObjectURL(data)
+      console.log("chemin : ", url)
+      setAvatarUrl(url)
+
+    } catch (error) {
+      console.log('Error downloading image: ', error)
+    }
+  }
   return (
   <article class="flex bg-white transition hover:shadow-xl w-2/5">
     <div class="rotate-180 p-2 [writing-mode:_vertical-lr]">
@@ -8,39 +67,40 @@ const Article_cards = (title, content) => {
         datetime="2022-10-10"
         class="flex items-center justify-between gap-4 text-xs font-bold uppercase text-gray-900"
       >
-        <span>2022</span>
+        <span>{getYear(post.article.created_at)}</span>
         <span class="w-px flex-1 bg-gray-900/10"></span>
-        <span>Oct 10</span>
+        <span>{getMonth(post.article.created_at)} {getDay(post.article.created_at)}</span>
       </time>
     </div>
 
     <div class="hidden sm:block sm:basis-56">
       <img
         alt="Guitar"
-        src="https://images.unsplash.com/photo-1609557927087-f9cf8e88de18?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+        src={downloadImage(post.article.image) ? avatarUrl : ""}
+
         class="aspect-square h-full w-full object-cover"
       />
     </div>
 
     <div class="flex flex-1 flex-col justify-between">
       <div class="border-l border-gray-900/10 p-4 sm:border-l-transparent sm:p-6">
-        <a href="#">
+      <a href={`/articles/${post.article.id_article}`}>
           <h3 class="font-bold uppercase text-gray-900">
-            {title}
+            {post.article.title}
           </h3>
         </a>
 
         <p class="mt-2 text-sm leading-relaxed text-gray-700 line-clamp-3">
-          {content}
+          {post.article.content}
         </p>
       </div>
 
       <div class="sm:flex sm:items-end sm:justify-end">
         <a
-          href="#"
+          href={`/articles/${post.article.id_article}`}
           class="block bg-yellow-300 px-5 py-3 text-center text-xs font-bold uppercase text-gray-900 transition hover:bg-yellow-400"
         >
-          Read Blog
+          Read Article
         </a>
       </div>
     </div>
--- Blog-ECE/pages/articles/[id].js
+++ Blog-ECE/pages/articles/[id].js
@@ -1,22 +1,81 @@
 import { useRouter } from 'next/router'
-import ReactMarkdown from 'react-markdown'
+import { useState } from "react";
 import parse from "html-react-parser";
 import { supabase } from '../api/api'
+import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react'
+import { Tiptap } from '../../components/editor/Tiptap';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function Post({ article }) {
 
+  const [description, setDescription] = useState("")
+  const [idArticle,setidArticle] = useState(null)
+  const [username,setUsername] = useState(null)
+  const supabase = useSupabaseClient()
+  const user = useUser()
+  const [loading, setLoading] = useState(true)
   const router = useRouter()
   if (router.isFallback) {
     return <div>Loading...</div>
   }
+
+  async function getUser(article){
+
+    const { data } = await supabase
+    .from('profiles')
+    .select('username')
+    .filter('id', 'eq', article.id_user)
+    .single()
+
+    setUsername(data.username)
+    setidArticle(article.id_article)
+  }
+
+  async function createComment({ description }) {
+    try {
+
+      setLoading(true)
+
+      const insert = {
+        id_comment: uuidv4(),
+        created_at: new Date().toISOString(),
+        content: description,
+        id_user: user.id,
+        id_article: idArticle,
+      }
+
+      console.log("Insertion", insert)
+
+      let { error } = await supabase.from('comments').insert(insert)
+      if (error) throw error
+      alert('Comment created!')
+    } catch (error) {
+      alert('Error inserting the data!')
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
     <div className=''>
       <h1 className="text-5xl mt-4 font-semibold tracking-wide text-center">{article.title}</h1>
-      <p className="text-sm font-light my-4 text-center">written by {article.id_user}</p>
+      <p className="text-sm font-light my-4 text-center">written by {getUser(article) ? username : "Anonyme"}</p>
       <div className="mt-8 mb-4 prose m-auto">
-        {/* <ReactMarkdown className='prose' children={article.content} /> */}
-        {parse(article.content)}
+        {parse(article.content) }
       </div>
+      <div className='App'>
+      {user ? <h2 className='m-auto prose mb-4'>Post a comment here...</h2> : ""}
+      {user ? <Tiptap setDescription={setDescription} /> : <div className='prose'>You can't post comment if you're not logged in...</div>}
+      </div>
+      {user ? <div className="flex pb-20 justify-center">
+        <button
+          className="inline-flex justify-center py-3 px-6 border border-transparent shadow text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+          onClick={() => createComment({ description })}
+        >
+          Post
+        </button>
+      </div> : ""}
     </div>
   )
 }
@@ -44,16 +103,9 @@ export async function getStaticProps({ params }) {
     .filter('id_article', 'eq', id)
     .single()
 
-  // const { data2 } = await supabase
-  // .from('profiles')
-  // .select('username')
-  // .filter('id', 'eq', data.id_user)
-  // .single()
-
   return {
     props: {
       article: data,
-      // user: data2
     }
   }
 }
