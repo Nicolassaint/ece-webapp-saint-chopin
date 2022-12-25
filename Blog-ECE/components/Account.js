146d8093a1241bb16d72b672ca0a27cbc96366a2
nicolassaint <nicolas.saint78@gmail.com>
2022-12-25 21:57:39 +0100

    fix: lbug

--- Blog-ECE/components/Account.js
+++ Blog-ECE/components/Account.js
@@ -67,7 +67,6 @@ export default function Account({ session }) {
       if (error) throw error
       alert('Profile updated!')
     } catch (error) {
-      alert('Error updating the data!')
       console.log(error)
     } finally {
       setLoading(false)
