f7ca6b37993180df5b67d98dcd6ea3abe5dbce8d
thomas91600 <thomas.chopin@edu.ece.fr>
2022-12-27 20:28:22 +0100

    feat: about us

--- Blog-ECE/pages/about.js
+++ Blog-ECE/pages/about.js
@@ -10,14 +10,14 @@ const contacts = () => {
       <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
         <div class="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
           <div className="md:5/12 lg:w-6/12">
-            <h2 className="text-2xl text-gray-900 font-bold md:text-4xl dark:text-white">PDG: Nicolas Saint</h2>
+            <h2 className="text-2xl text-gray-900 font-bold md:text-4xl dark:text-white">Nicolas Saint</h2>
             <Image src={mypic1} alt="Nicolas Saint" width="150px" height="150px" />
             <h3 className="text-2xl text-gray-900 md:text-2xl dark:text-white">Phone : 06 65 48 62 68</h3>
             <h3 className="text-2xl text-gray-900 md:text-2xl dark:text-white"> Email : nicolas.saint78@gmail.com</h3>
           </div>
 
           <div className="md:10/12 lg:w-6/12">
-            <h2 className="text-2xl text-gray-900 font-bold md:text-4xl dark:text-white">Assitant: Thomas Chopin</h2>
+            <h2 className="text-2xl text-gray-900 font-bold md:text-4xl dark:text-white">Thomas Chopin</h2>
             <Image src={mypic2} alt="Nicolas Saint" width="150px" height="150px" />
             <h3 className="text-2xl text-gray-900 md:text-2xl dark:text-white">Phone : 06 65 48 62 68</h3>
             <h3 className="text-2xl text-gray-900 md:text-2xl dark:text-white"> Email : thomas.chopin@edu.ece.fr</h3>
